5eb4b8848a28eb275885332df36ae1e211cf3016
Guilherme Neves Thomaz <gnthomaz@gmail.com>
2024-04-11 12:52:41 -0300

    second commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,15 +1,37 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
-        return Controller.extend("zcursoappgnt.controller.View1", {
-            onInit: function () {
+        //return Controller.extend("zcursoappgnt.controller.View1", {
+        //onInit: function () {
 
+        //},
+
+        //});
+
+        var PageController = Controller.extend("zcursoappgnt.controller.View1", {
+
+            onPress: function (evt) {
+                debugger;
+                MessageToast.show(evt.getSource().getId() + " Pressed");
+            },
+
+            onPress2: function (evt) {
+                debugger;
+                var varvalue = this.byId("_IDGenInput1").getValue();
+                this.byId("_IDGenInput1").setValue("Some thing");
+                MessageToast.show(evt.getSource().getId() + " Wello world");
             }
+
         });
+
+        return PageController;
+
     });
